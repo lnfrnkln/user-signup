3833c2e523e8be41d02136d3c83854693e28502d
Lakisha Howard <lnfrnkln@gmail.com>
2019-04-23 20:20:15 -0500

    revised

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
-from flask import Flask, request,render_template
-import cgi
+from flask import Flask, render_template, redirect, request,url_for
+
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/register", methods=['POST'])
-def register():
-   username = cgi.escape(request.form['username'])
-   password = cgi.escape(request.form['password'])
-   password2 = cgi.escape(request.form['password2'])
-   email=cgi.escape(request.form['email'])
+@app.route("/", methods=['GET','POST'])
+def home():
+    if request.method =='post':
+        username = request.form['username']
+        password = request.form['password']
+        password2 =request.form['password2']
+        email=request.form['email']
+        username_error =""
+        password_error = ""
+        password2_error =""
+        email_error=""
 
-   usernameError =""
-   passwordError = ""
-   password2Error =""
-   emailError=""
+        if username =='':
+            username_error = "Username is required."
+        elif " " in username:
+            username_error ="Username contains spaces."
+        elif len(username) < 3 or len(username) >20:
+            username_error="Username must be 3-20 characters long."
+     
+        if password =='':
+            password_error ="Password required."
+        elif " " in password:
+            password_error= "Password contains spaces."
+        elif len(password) <3 or len (password) > 20:
+            password_error= "Password must be 3-20 characters long."
 
-   if not username:
-       print("no username")
-       usernameError = "Username is required"
-   if not password:
-       passwordError = "Password is required"
-   if len(password) < 3:
-       passwordError = "Password must be at least 3 characters long"
-   if len(password) >20:
-       passwordError="password must be no more than 20 charadcters long"
-   else:
-       hasNumber = False
-       for char in password:
-           if char.isdigit():
-               hasNumber = True
-       if not hasNumber:
-           passwordError = "Password must contain a number"
-   if password  != password2:
-       password2Error = "Password 2 must match password"
-   if "@" and "." not in email:
-       emailError="email must contain @ and ."
+        if password2 == '':
+            password2_error = "Please confirm your password."
+        elif password2 != password:
+            password2_error = "You password doesn't match."
 
-   if usernameError or passwordError or password2Error or emailError:
-       print("there was an error!")
-       return render_template('home_page.html',username=username,usernameError=usernameError,password=password,passwordError=passwordError,password2=password2,password2Error=password2Error,email=email,emailError=emailError)
-   return "Welcome, " + username
+        if email == '':
+            email_error = ''
+        elif "@" not in email:
+            email_error = "Your email address is missing @ symbol."
+        elif "." not in email:
+            email_error = "Your email address is missing . symbol."
+        elif " " in email:
+            email_error = "Email address cannot contain spaces."
+        elif len(email) < 3 or len(email) > 20:
+            email_error = "Your email address must be at least 3 and shorter than 20."
+        
+        if not username_error and not password_error and not password2_error and not email_error:
+            return redirect(url_for("welcome_page", username=username))
+            
+        else:
+            return render_template("home_page.html", username = username, email = email, username_error = username_error, password_error = password_error, password2_error = password2_error, email_error = email_error, password=password, password2=password2)
+
+    
+    return render_template("home_page.html")
 
 
-@app.route("/")
-def index():
-   return render_template('welcome_page.html')
+@app.route("/welcome")
+def welcome():
+    username=request.args.get('username')
+    email =request.args.get('email')
+    return render_template("welcome_page.html", name=username, email=email)
 
-@app.route("/register", methods=['GET'])
-def register_page():
-   return render_template('home_page.html')
 
 app.run()
+
+
